ae90be040d887fe6e7b794b59e9d8f9681dc31d8
Ramos <andfera201@gmail.com>
2025-06-18 11:46:35 -0500

    @PatchMapping

--- src/main/java/courseSpringBoot/controllers/CustomerController.java
+++ src/main/java/courseSpringBoot/controllers/CustomerController.java
@@ -64,4 +64,26 @@ public class CustomerController {
         }
         return null;
     }
+
+    @PatchMapping("/clients")
+    public Customer patchClient(@RequestBody Customer customer) {
+        for (Customer c : customers) {
+            if (c.getId() == customer.getId()) {
+                if (customer.getName() != null) {
+                    c.setName(customer.getName());
+                }
+
+                if (customer.getUsername() != null) {
+                    c.setUsername(customer.getUsername());
+                }
+
+                if (customer.getPassword() != null) {
+                    c.setPassword(customer.getPassword());
+                }
+
+                return c;
+            }
+        }
+        return null;
+    }
 }
