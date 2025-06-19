5cf26427015db64a211bb9ddb3641e65fd2ef152
Ramos <andfera201@gmail.com>
2025-06-19 08:46:48 -0500

    Return URI (POST)

--- src/main/java/courseSpringBoot/controllers/CustomerController.java
+++ src/main/java/courseSpringBoot/controllers/CustomerController.java
@@ -4,7 +4,9 @@ import courseSpringBoot.domain.Customer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -46,9 +48,14 @@ public class CustomerController {
     @PostMapping
     public ResponseEntity<?> postClient(@RequestBody Customer customer) {
         customers.add(customer);
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body("Client: '" + customer.getName() + "' created successfully.");
+        // return ResponseEntity.status(HttpStatus.CREATED).body("Client: '" + customer.getName() + "' created successfully.");
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{username}")
+                .buildAndExpand(customer.getUsername())
+                .toUri();
+        // return ResponseEntity.created(location).build();
+        return ResponseEntity.created(location).body(customer);
     }
 
     // @RequestMapping(method = RequestMethod.PUT)
